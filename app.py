5c7f69f897f5b29292a8f7c2213bf16fe82471aa
Madiha Musfira <madihamusfira143@gmail.com>
2026-03-17 23:39:28 +0530

    Initial commit - AI Resume Screening App

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+
+st.title("AI Resume Screening System")
+
+# Load dataset
+df = pd.read_csv("resume.csv")
+
+st.subheader("Resume Dataset")
+st.dataframe(df)
+
+# Input job skills
+job_skills = st.text_input("Enter required skills (comma separated)")
+
+if st.button("Find Best Candidates"):
+
+    if job_skills == "":
+        st.warning("Please enter required skills")
+    else:
+        required = [x.strip().lower() for x in job_skills.split(",")]
+
+        scores = []
+
+        for index, row in df.iterrows():
+            candidate_skills = [x.strip().lower() for x in row["Skills"].split(",")]
+            match = len(set(required) & set(candidate_skills))
+            scores.append(match)
+
+        df["MatchScore"] = scores
+
+        ranked = df.sort_values(by="MatchScore", ascending=False)
+
+        st.subheader("Top Candidates")
+        st.dataframe(ranked)
+
+        # download result
+        st.download_button(
+            "Download Results",
+            ranked.to_csv(index=False),
+            file_name="ranked_candidates.csv",
+            mime="text/csv"
+        )
